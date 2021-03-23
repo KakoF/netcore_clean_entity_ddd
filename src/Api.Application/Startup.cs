c5187e64ae76e186ab0430399b03bef9e0be5f35
Marcos <kakoferrare87@gmail.com>
2021-03-23 16:56:28 -0300

    init

---
+++ src/Api.Application/Startup.cs
@@ -0,0 +1,86 @@
+
+using System;
+using Api.CrossCutting.DependencyInjection;
+using Api.Domain.Security;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment HostingEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            ConfigureService.ConfigureDependenciesService(services);
+            ConfigureRepository.ConfigureDependenciesRepository(services);
+
+            var signinConfiguration = new SigninConfigurations();
+            services.AddSingleton(signinConfiguration);
+
+            var tokenConfiguration = new TokenConfiguration();
+            new ConfigureFromConfigurationOptions<TokenConfiguration>(
+                Configuration.GetSection("TokenConfiguration")).Configure(tokenConfiguration);
+
+            services.AddSingleton(tokenConfiguration);
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Clean Architecture c# DDD",
+                    Description = "Arquitetura DDD",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Kako",
+                        Email = "kakoferrare87@gmail.com",
+                        Url = new Uri("https://github.com/KakoF")
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Clean Arch DDD NetCore3.1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ src/Api.Service/Services/LoginService.cs
@@ -0,0 +1,94 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Api.Domain.Dtos;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+using Api.Domain.Repository;
+using Api.Domain.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Api.Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IUserRepository _repository;
+        private readonly TokenConfiguration _tokenConfiguration;
+        private readonly SigninConfigurations _signinConfigurations;
+
+        private IConfiguration _configuration { get; }
+        public LoginService(IUserRepository repository, TokenConfiguration tokenConfiguration, SigninConfigurations signinConfigurations, IConfiguration configuration)
+        {
+            _repository = repository;
+            _tokenConfiguration = tokenConfiguration;
+            _signinConfigurations = signinConfigurations;
+            _configuration = configuration;
+        }
+        public async Task<object> Login(LoginDto user)
+        {
+            var baseUser = new UserEntity();
+            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                baseUser = await _repository.Login(user.Email);
+                if (baseUser == null)
+                {
+                    return new
+                    {
+                        authenticated = false,
+                        message = "Falha ao autenticar"
+                    };
+                }
+
+                var identity = new ClaimsIdentity(
+                    new GenericIdentity(user.Email),
+                    new[]{
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.UniqueName, user.Email),
+                    }
+                );
+                DateTime createdDate = DateTime.Now;
+                DateTime expirationDate = createdDate + TimeSpan.FromSeconds(_tokenConfiguration.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var token = CreateToken(identity, createdDate, expirationDate, handler);
+                return SuccessObject(createdDate, expirationDate, token, user);
+
+
+            }
+
+            return null;
+        }
+
+
+        private string CreateToken(ClaimsIdentity identity, DateTime createdDate, DateTime expirationDate, JwtSecurityTokenHandler handler)
+        {
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _tokenConfiguration.Issuer,
+                Audience = _tokenConfiguration.Audience,
+                SigningCredentials = _signinConfigurations.SigningCredentials,
+                Subject = identity,
+                NotBefore = createdDate,
+                Expires = expirationDate,
+            });
+
+            var token = handler.WriteToken(securityToken);
+            return token;
+        }
+        private object SuccessObject(DateTime createdDate, DateTime expirationDate, string token, LoginDto user)
+        {
+            return new
+            {
+                authenticated = true,
+                created = createdDate,
+                expiration = expirationDate,
+                accessToken = token,
+                userName = user.Email,
+                message = "Usuário logado com sucesso.",
+            };
+        }
+    }
+}
